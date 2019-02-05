1b672a8d5ff9d32e90dfcc8f39f4b3663c4de74a
cvols <chris.volonnino@gmail.com>
2019-02-05 17:44:07 -0500

    created a duel from find a league

--- src/loggedin/screens/Duel/Duel.js
+++ src/loggedin/screens/Duel/Duel.js
@@ -1,28 +1,174 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import store from '../../../redux/store';
 import { connect } from 'react-redux';
 
+// import store from '../../../redux/store';
+import Screen from '../../../ui/components/Screen';
+import Question from '../../../ui/components/Question';
+import Answers from '../../../ui/components/Answers';
+
 class Duel extends React.Component {
-	render() {
-		const {quizData} = this.props
-		console.log('Duel this.props: ', this.props);
-		return (
-			<View>
-				<Text>Duel</Text>
-			</View>
-		)
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      nr: 0,
+      isAnswered: false,
+      gameOver: false,
+      score: 0,
+      total: 0,
+      question: '',
+      incorrectAnswers: [],
+      correctAnswer: [],
+      category: [],
+      questions: [],
+    }
+
+    this.nextQuestion = this.nextQuestion.bind(this);
+    this.handleClickButton = this.handleClickButton.bind(this);
+    this.increaseScore = this.increaseScore.bind(this);
+    this.shuffle = this.shuffle.bind(this);
+  }
+
+  async componentWillMount() {
+    const { quizData } = this.props;
+    const { nr } = this.state;
+
+    this.setState({
+      questions: quizData._value,
+      question: quizData._value.questions[nr].question,
+      correctAnswer: quizData._value.questions[nr].correct_answer,
+      incorrectAnswers: [quizData._value.questions[nr].correct_answer, quizData._value.questions[nr].incorrect_answers[0], quizData._value.questions[nr].incorrect_answers[1], quizData._value.questions[nr].incorrect_answers[2]],
+      total: quizData._value.questions.length,
+      category: quizData._value.questions[nr].category,
+      nr: this.state.nr + 1,
+    }, () => {
+      this.shuffle();
+    });
+  }
+  shuffle() {
+    const { incorrectAnswers } = this.state;
+    const array = incorrectAnswers;
+
+    function shuffle(array) {
+      let currentIndex = array.length, temporaryValue, randomIndex;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+    }
+
+    shuffle(array);
+
+    this.setState({
+      incorrectAnswers: array
+    });
+  }
+
+  nextQuestion() {
+    const { nr, total } = this.state;
+
+    if (nr === total) {
+      this.setState({ gameOver: true });
+    } else {
+      this.pushData(nr);
+      this.setState({ isAnswered: false });
+      this.shuffle();
+    }
+  }
+
+  pushData(nr) {
+    const { questions } = this.state;
+
+    this.setState({
+      question: questions.questions[nr].question,
+      incorrectAnswers: [questions.questions[nr].correct_answer, questions.questions[nr].incorrect_answers[0], questions.questions[nr].incorrect_answers[1], questions.questions[nr].incorrect_answers[2]],
+      correctAnswer: questions.questions[nr].correct_answer,
+      category: questions.questions[nr].category,
+      nr: this.state.nr + 1,
+    });
+  }
+
+  handleClickButton() {
+    this.setState({ isAnswered: true });
+  }
+
+  increaseScore() {
+    const { score } = this.state;
+
+    this.setState({
+      score: score + 1,
+    });
+  }
+
+  gameStart() {
+    const { nr, total, isAnswered, incorrectAnswers, correctAnswer, category, question, questions } = this.state;
+
+    return (
+      <View>
+        {questions ?
+          <View>
+            <Question
+              category={category}
+              nr={nr}
+              total={total}
+              question={question}
+            />
+            <Answers
+              incorrectAnswers={incorrectAnswers}
+              correctAnswer={correctAnswer}
+              nextQuestion={this.nextQuestion}
+              isAnswered={isAnswered}
+              increaseScore={this.increaseScore}
+              clickButton={this.handleClickButton}
+              shuffle={this.shuffle}
+            />
+          </View>
+          : null
+        }
+      </View>
+    )
+  }
+
+  gameOver() {
+    const { score, total } = this.state;
+
+    return (
+      <View>
+        <Text>Game Over Man</Text>
+        <Text>You got {score} of {total} questions right</Text>
+      </View>
+    )
+  }
+
+  render() {
+    const { gameOver } = this.state;
+
+    return (
+      <Screen>
+        {gameOver ? this.gameOver() : this.gameStart()}
+      </Screen>
+    )
+  }
 }
 
 const mapStateToProps = (state, props) => {
-	console.log('mapStateToProps', state);
-	console.log('props', props);
-        
-	return {
-	    quizData = state.quizData   
-	}
+  console.log('mapStateToProps', state);
+
+  return {
+    quizData: state.quizData
+  }
 }
 
-export default connect(mapStateToProps,{})(Duel);
+export default connect(mapStateToProps, {})(Duel);
 
